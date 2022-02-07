5f9b5ec1f320ca26b41809a9c6b523a3572148f2
agathesf <agathe.saintfrison@gmail.com>
2022-02-07 16:03:39 +0100

    add eyes on profile

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+
   var cursor = document.querySelector(".custom-cursor");
   var links = document.querySelectorAll("a");
   var initCursor = false;
+  var page = document.querySelector("html");
+  var eyes = document.querySelector(".eyes");
 
   for (var i = 0; i < links.length; i++) {
     var selfLink = links[i];
@@ -38,4 +41,70 @@ document.addEventListener("DOMContentLoaded", function(event) {
     });
     initCursor = false;
   };
+
+  page.addEventListener("mouseleave", function() {
+    // highlight the mouseout target
+    eyes.classList.add("orange");
+
+    // reset the color after a short delay
+    setTimeout(function() {
+      eyes.target.style.color = "";
+    }, 500);
+  }, false);
+
+  page.addEventListener("mouseenter", function() {
+    // highlight the mouseout target
+    eyes.classList.remove("orange");
+
+    // reset the color after a short delay
+    setTimeout(function() {
+      eyes.target.style.color = "";
+    }, 500);
+  }, false);
+
+  // console.log("hi :)");
+
+  let eyeBall = document.querySelector(".eyeball"),
+      pupil = document.querySelector(".pupil"),
+      eyeArea = eyeBall.getBoundingClientRect(),
+      pupilArea = pupil.getBoundingClientRect(),
+      R = 57,
+      r = pupilArea.width/2,
+      centerX = eyeArea.left + R,
+      centerY = eyeArea.top + R;
+
+  document.addEventListener("mousemove", (e)=>{
+    let x = e.clientX - centerX,
+        y = e.clientY - centerY,
+        theta = Math.atan2(y,x),
+        angle = theta*180/Math.PI + 360;
+    
+    
+    pupil.style.transform = `translateX(${R - r +"px"}) rotate(${angle + "deg"})`;
+    pupil.style.transformOrigin = `${r +"px"} center`;
+  });
+
+  let eyeBallBis = document.querySelector(".eyeball-bis"),
+      pupilBis = document.querySelector(".pupil-bis"),
+      eyeAreaBis = eyeBallBis.getBoundingClientRect(),
+      pupilAreaBis = pupilBis.getBoundingClientRect(),
+      Rb = 57,
+      rb = pupilAreaBis.width/2,
+      centerXb = eyeAreaBis.left + Rb,
+      centerYb = eyeAreaBis.top + Rb;
+
+  document.addEventListener("mousemove", (e)=>{
+    let xb = e.clientX - centerXb,
+        yb = e.clientY - centerYb,
+        thetab = Math.atan2(yb,xb),
+        angleb = thetab*180/Math.PI + 360;
+    
+    
+    pupilBis.style.transform = `translateX(${Rb - rb +"px"}) rotate(${angleb + "deg"})`;
+    pupilBis.style.transformOrigin = `${rb +"px"} center`;
+  });
+
+
+
 });
+
